1ce088ca5d2c75fa5ee1573054ac9685fcd1321d
dExIT <dexit@dyc.lv>
2023-03-20 21:05:12 +0000

    added cards js and weather api base

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,1 +1,15 @@
 // base code for the cards
+
+function generateCard(data) {
+
+var cardBase = `<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+  <div class="card-header">${data.header}</div>
+  <div class="card-body">
+    <h5 class="card-title">${data.title}</h5>
+    <p class="card-text">${data.content}</p>
+  </div>
+</div>
+`;
+
+  return cardBase;
+}
--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -1,1 +1,8 @@
 // code base for weather api
+// https://openweathermap.org/api fetch forecast for 5 days
+
+function getForecast(city,dateToday = new Date().toISOString().slice(0,10)) {
+    const apiKey = "ad9ca655ad513633f73afcce5f7d7aad";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&cnt=5&dt=${dateToday}`;
+    return fetch(url);
+}
