a5607aaec7d0c34e789c38b489385d59323f6d6e
dExIT <dexit@dyc.lv>
2023-03-21 09:30:20 +0000

    Got the results showing up

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -2,7 +2,7 @@
 // https://openweathermap.org/api fetch forecast for 5 days
 
 function getForecast(city,dateToday = new Date().toISOString().slice(0,10)) {
-    const apiKey = "ad9ca655ad513633f73afcce5f7d7aad";
+    const apiKey = "e32dadadb09daca70089121cca0a5169";
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&dt=${dateToday}`;
    
     return fetch(url);
@@ -11,7 +11,7 @@ function getForecast(city,dateToday = new Date().toISOString().slice(0,10)) {
    // return fetch(url);
 
    function getForecast5day(city) {
-    const apiKey = "ad9ca655ad513633f73afcce5f7d7aad";
+    const apiKey = "e32dadadb09daca70089121cca0a5169";
     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=5&appid=${apiKey}`; //dt=${dateToday}`;
    
     return fetch(url);
